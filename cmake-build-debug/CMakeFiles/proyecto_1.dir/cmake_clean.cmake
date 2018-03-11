18d57f3d1741d1a10fbdeefe925d1680bf624ad4
Michael Gonzalez <michael.gonzalez1812@gmail.com>
2018-03-10 22:02:18 -0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/proyecto_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyecto_1.dir/main.cpp.o"
+  "proyecto_1.pdb"
+  "proyecto_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyecto_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
